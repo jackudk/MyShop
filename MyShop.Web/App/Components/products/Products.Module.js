42948c8df9cd8f529af580cb8fbf3cd0abe5a28b
JackuDK-PC\JackuDK <longaka07110@gmail.com>
2016-09-30 08:36:04 +0700

    Config angular-ui-router

---
+++ MyShop.Web/App/Components/products/Products.Module.js
@@ -0,0 +1,17 @@
+﻿
+(function () {
+    angular.module('myshop.products', ['myshop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', {
+            url: '/products',
+            templateUrl: '/App/Components/products/productListView.html',
+            controller: 'productListController'
+        }).state('product_add', {
+            url: '/product_add',
+            templateUrl: '/App/Components/products/productAddView.html',
+            controller: 'productAddController'
+        });
+    }
+})();
--- MyShop.Web/App/app.js
+++ MyShop.Web/App/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿/// <reference path="/Assets/admin/libs/angular/angular.js" />
+/// <reference path="/Assets/admin/libs/angular-ui-router/release/angular-ui-router.js" />
+
+(function () {
+    angular.module('myshop', ['myshop.products', 'myshop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider,$urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: '/admin',
+            templateUrl: '/App/Components/home/homeView.html',
+            controller:'homeController'
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
