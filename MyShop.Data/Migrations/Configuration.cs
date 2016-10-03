9b0b2977a1cecd270154e52949a6b9a76417ae21
JackuDK-PC\JackuDK <longaka07110@gmail.com>
2016-10-03 22:41:39 +0700

    add Filter

--- MyShop.Data/Migrations/Configuration.cs
+++ MyShop.Data/Migrations/Configuration.cs
@@ -1,10 +1,9 @@
-﻿namespace MyShop.Data.Migrations
+namespace MyShop.Data.Migrations
 {
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
     using Model.Models;
     using System;
-    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
@@ -18,49 +17,30 @@
         protected override void Seed(MyShopDbContext context)
         {
             //  This method will be called after migrating to the latest version.
-            CreateProductCategoriesSample(context);
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new MyShopDbContext()));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new MyShopDbContext()));
 
-
-            //var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new MyShopDbContext()));
-            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new MyShopDbContext()));
-
-            //var user = new ApplicationUser()
-            //{
-            //    UserName = "long",
-            //    Email = "long@gmail.com",
-            //    BirthDay = DateTime.Now,
-            //    EmailConfirmed = true,
-            //    FullName = "le long"
-            //};
-
-            ////Add new user
-            //userManager.Create(user, "12345@");
-            ////Add new roles
-            //if (!roleManager.Roles.Any())
-            //{
-            //    roleManager.Create(new IdentityRole { Name = "Admin" });
-            //    roleManager.Create(new IdentityRole { Name = "User" });
-            //}
-
-            ////Add user-roles
-            //var adminUser = userManager.FindByEmail("long@gmail.com");
-            //userManager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
-        }
-
-        private void CreateProductCategoriesSample(MyShopDbContext context)
-        {
-            if (context.ProductCategories.Count() == 0)
+            var user = new ApplicationUser()
             {
-                List<ProductCategory> lstProductCategory = new List<ProductCategory>()
-                {
-                    new ProductCategory(){Name="Đồ điện lạnh", Alias="do-dien-lanh", Status=true },
-                    new ProductCategory(){Name="Đồ điện tử", Alias="do-dien-tu", Status=true },
-                    new ProductCategory(){Name="Đồ gia dụng", Alias="do-gia-dung", Status=true },
-                    new ProductCategory(){Name="Đồ thời trang", Alias="do-thoi-trang", Status=true }
-                };
-                context.ProductCategories.AddRange(lstProductCategory);
-                context.SaveChanges();
+                UserName = "long",
+                Email = "long@gmail.com",
+                BirthDay = DateTime.Now,
+                EmailConfirmed = true,
+                FullName = "le long"
+            };
+
+            //Add new user
+            userManager.Create(user, "12345@");
+            //Add new roles
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
             }
+
+            //Add user-roles
+            var adminUser = userManager.FindByEmail("long@gmail.com");
+            userManager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
         }
     }
 }
